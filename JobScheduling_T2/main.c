315ea5912cd96d16542a3405f3c1d6debfcc947a
Nakagawadesu <nukitaro@outlook.com>
2024-08-12 19:10:05 -0300

    Refactor main.c for job scheduling tasks

--- JobScheduling_T2/main.c
+++ JobScheduling_T2/main.c
@@ -1,10 +1,11 @@
-#include "task.h"
-#include "queue.h"
-#include "FCFS.h"
-#include "SJF.h"
+
+#include "scheduling.h"
+//#include "FCFS.h"
 #include <stdio.h>
 
 int main(){
+
+
     FILE *file = fopen("daTask.txt", "r");
     if (file == NULL) {
         printf("Error: Could not open file.\n");
@@ -25,14 +26,57 @@ int main(){
         int priority, arrival, waiting;
      
         fscanf(file, "Task %*d: Priority: %d, Arrival: %d, Waiting: %d\n", &priority, &arrival, &waiting);
-        tasks[i] = *createTask(priority, arrival, waiting);
+        tasks[i] = *createTask( i, priority, arrival, waiting);
     }
 
     fclose(file);  
 
+    
     printTasks(tasks, n);  
+    
+
+    Solution *solutions = SJF(tasks, n);
+    printf("\033[35m SJF - solution \n");
+    for(int i = 0; i < n; i++){
+        printf("Task %d: Started: %d, Ended: %d\n", solutions[i].id, solutions[i].started, solutions[i].ended);
+    }
+
+
+        printf("\n");
+        printf("\n");
+
+    Solution *solutions2 = FCFS(tasks, n);
+    printf("\033[35m FCFS - solution \n");
+    for(int i = 0; i < n; i++){
+      
+        printf("Task %d: Started: %d, Ended: %d\n", solutions2[i].id, solutions2[i].started, solutions2[i].ended);
+    }
 
     free(tasks);
+
+    // write solution and solution 2 to different files
+    FILE *file1 = fopen("SJF.txt", "w");
+    if (file1 == NULL) {
+        printf("Error: Could not open file.\n");
+        return 1;
+    }
+    for(int i = 0; i < n; i++){
+        fprintf(file1, "Task %d: Started: %d, Ended: %d\n", solutions[i].id, solutions[i].started, solutions[i].ended);
+    }
+    fclose(file1);
+    free(solutions);
+
+
+    FILE *file2 = fopen("FCFS.txt", "w");
+    if (file2 == NULL) {
+        printf("Error: Could not open file.\n");
+        return 1;
+    }
+    for(int i = 0; i < n; i++){
+        fprintf(file2, "Task %d: Started: %d, Ended: %d\n", solutions2[i].id, solutions2[i].started, solutions2[i].ended);
+    }
+    fclose(file2);
+    free(solutions2);
 
     return 0;
 }
