2ec782ee0657fb6fdce3ed48cb8130bd43071087
Nakagawadesu <nukitaro@outlook.com>
2024-08-14 17:08:28 -0300

    Refactor main.c file for improved readability and add Round Robin scheduling algorithm

--- JobScheduling_T2/main.c
+++ JobScheduling_T2/main.c
@@ -1,102 +1,125 @@
 
 #include "scheduling.h"
-//#include "FCFS.h"
+// #include "FCFS.h"
 #include <stdio.h>
 
-int main(){
-
+int main()
+{
 
     FILE *file = fopen("daTask.txt", "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error: Could not open file.\n");
         return 1;
     }
 
     int n = 0;
-    char line[100];  
+    char line[100];
 
-    while (fgets(line, sizeof(line), file)) {
+    while (fgets(line, sizeof(line), file))
+    {
         n++;
     }
-    rewind(file);  
+    rewind(file);
 
-    Task *tasks = InitTask(n); 
+    Task *tasks = InitTask(n);
 
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         int priority, arrival, waiting;
-     
+
         fscanf(file, "Task %*d: Priority: %d, Arrival: %d, Waiting: %d\n", &priority, &arrival, &waiting);
-        tasks[i] = *createTask( i, priority, arrival, waiting);
+        tasks[i] = *createTask(i, priority, arrival, waiting, 0);
     }
 
-    fclose(file);  
+    fclose(file);
 
-    
-    printTasks(tasks, n);  
-    
+    printTasks(tasks, n);
 
     Solution *solutions = SJF(tasks, n);
     printf("\033[35m SJF - solution \n");
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         printf("Task %d: Started: %d, Ended: %d\n", solutions[i].id, solutions[i].started, solutions[i].ended);
     }
 
-
-        printf("\n");
-        printf("\n");
+    printf("\n");
+    printf("\n");
 
     Solution *solutions2 = FCFS(tasks, n);
     printf("\033[35m FCFS - solution \n");
-    for(int i = 0; i < n; i++){
-      
+    for (int i = 0; i < n; i++)
+    {
+
         printf("Task %d: Started: %d, Ended: %d\n", solutions2[i].id, solutions2[i].started, solutions2[i].ended);
     }
 
-
     Solution *solutions3 = priorityBased(tasks, n);
     printf("\033[35m PriorityBased - solution \n");
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         printf("Task %d: Started: %d, Ended: %d\n", solutions3[i].id, solutions3[i].started, solutions3[i].ended);
+    }
+
+    Solution *solutions4 = RoundRobin(tasks, n, 1);
+    printf("\033[35m RR - solution \n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("Task %d: Started: %d, Ended: %d\n", solutions4[i].id, solutions4[i].started, solutions4[i].ended);
     }
 
     free(tasks);
 
     FILE *file1 = fopen("SJF.txt", "w");
-    if (file1 == NULL) {
+    if (file1 == NULL)
+    {
         printf("Error: Could not open file.\n");
         return 1;
     }
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         fprintf(file1, "Task %d: Started: %d, Ended: %d\n", solutions[i].id, solutions[i].started, solutions[i].ended);
     }
     fclose(file1);
     free(solutions);
 
-
     FILE *file2 = fopen("FCFS.txt", "w");
-    if (file2 == NULL) {
+    if (file2 == NULL)
+    {
         printf("Error: Could not open file.\n");
         return 1;
     }
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         fprintf(file2, "Task %d: Started: %d, Ended: %d\n", solutions2[i].id, solutions2[i].started, solutions2[i].ended);
     }
     fclose(file2);
     free(solutions2);
 
-
-
-   
     FILE *file3 = fopen("PriorityBased.txt", "w");
-    if (file3 == NULL) {
+    if (file3 == NULL)
+    {
         printf("Error: Could not open file.\n");
         return 1;
     }
-    for(int i = 0; i < n; i++){
-        fprintf(file3, "Task %d: Started: %d, Ended: %d\n", solutions2[i].id, solutions2[i].started, solutions2[i].ended);
+    for (int i = 0; i < n; i++)
+    {
+        fprintf(file3, "Task %d: Started: %d, Ended: %d\n", solutions3[i].id, solutions3[i].started, solutions3[i].ended);
     }
     fclose(file3);
-    free(solutions2);
+    free(solutions3);
+
+    FILE *file4 = fopen("RR.txt", "w");
+    if (file4 == NULL)
+    {
+        printf("Error: Could not open file.\n");
+        return 1;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        fprintf(file4, "Task %d: Started: %d, Ended: %d\n", solutions4[i].id, solutions4[i].started, solutions4[i].ended);
+    }
+    fclose(file4);
 
     return 0;
 }
